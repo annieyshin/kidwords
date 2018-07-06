6a6e1453ac05c738c50391d235e0ec62bc9ad214
Annie Shin <annieyshin@gmail.com>
2018-07-06 09:39:53 -0700

    add in Gemfile and initial content

---
+++ app.rb
@@ -0,0 +1,25 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/kids_words')
+
+#home page is the /
+get('/') do
+  @list = Item.all()
+  erb(:list)
+end
+
+#sending information
+post('/') do
+  name = params["name"]
+  item = Item.new(name)
+  item.save()
+  @list = Item.all()
+  erb(:list)
+end
+
+#gets information from items page
+get('/items/:id') do
+  @item = Item.find(params[:id])
+  erb(:item)
+end
