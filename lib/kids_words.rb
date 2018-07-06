2759ddc419962f3c1884a3e60d083d717affd8bb
Annie Shin <annieyshin@gmail.com>
2018-07-06 09:53:09 -0700

    Pass test 1

--- lib/kids_words.rb
+++ lib/kids_words.rb
@@ -20,11 +20,11 @@ class Word
     @@list.push(self)
   end
 
-  def self.find(id)
-    item_id = id.to_i()
+  def self.find(word)
+    word_id = word.to_i()
     @@list.each do |item|
-      if item.id == item_id
-        return item
+      if word.id == word_id
+        return word
       end
     end
   end
---
+++ spec/kids_words_spec.rb
@@ -0,0 +1,34 @@
+require('rspec')
+require('kids_words')
+
+describe("Word") do
+  before() do
+    Word.clear()
+  end
+
+  describe(".all") do
+    it("is empty at first") do
+      expect(Word.all()).to(eq([]))
+    end
+  end
+  #
+  # describe("#save") do
+  #   it("saves an item to the list of items") do
+  #     item = Item.new("tacos")
+  #     item.save()
+  #     expect(Item.all()).to(eq([item]))
+  #   end
+  # end
+
+  # describe(".clear") do
+  #   it("clears all items from the list") do
+  #     item = Item.new("tacos")
+  #     item.save()
+  #     Item.clear()
+  #     expect(Item.all()).to(eq([]))
+  #   end
+  # end
+
+
+  # end
+end
